e647935c50f561080e1dcd3b254c4bd3ef719d33
ErickGX <ericksaopaulino1@hotmail.com>
2024-11-07 15:01:29 -0300

    Alterações no estilo do card e do botao compra , e mudando espaçamento entre itens

--- js/modularizar.js
+++ js/modularizar.js
@@ -1,13 +1,19 @@
+// Função para carregar conteúdo HTML em um elemento específico
 function loadHTML(elementId, filePath) {
+    // Faz uma solicitação HTTP para buscar o conteúdo do arquivo especificado
     fetch(filePath)
-        .then(response => response.text())
+        .then(response => response.text()) // Converte a resposta para texto
         .then(data => {
+            // Insere o conteúdo obtido no elemento HTML identificado pelo ID
             document.getElementById(elementId).innerHTML = data;
         })
-        .catch(error => console.error('Erro ao carregar o arquivo:', error));
+        .catch(error => console.error('Erro ao carregar o arquivo:', error)); // Trata possíveis erros
 }
 
+// Adiciona um ouvinte de evento que dispara quando o DOM está completamente carregado
 document.addEventListener('DOMContentLoaded', () => {
+    // Carrega o conteúdo do arquivo headernav.html no elemento com ID "header"
     loadHTML('header', '/public/components/headernav.html');
+    // Carrega o conteúdo do arquivo footer.html no elemento com ID "footer"
     loadHTML('footer', '/public/components/footer.html');
 });
